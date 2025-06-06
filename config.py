05c73a7fe02602ba670a3e41093e3735f02344eb
julio <srmustashin@gmail.com>
2025-06-05 20:08:37 -0700

    Added MONGO_URI

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv()
 class Config:
     IWISP_API_KEY = os.getenv("IWISP_API_KEY")
     IWISP_API_URL = os.getenv("IWISP_API_URL")
+    MONGO_URI = os.getenv("MONGO_URI")
     SECRET_KEY = "clave_secreta"
     UPLOAD_FOLDER = "uploads"
